a3d114d8f44cd29d5fd60dc13b270807680de9e1
Sevan Keshish <35175803+sevankeshish@users.noreply.github.com>
2021-11-08 11:10:23 +0330

    API error :( , and the rest of the code

--- js/app.js
+++ js/app.js
@@ -1,20 +1,20 @@
 document.querySelector("#generate-names").addEventListener("submit", loadNames)
 
-function loadNames (e){
+function loadNames(e) {
     e.preventDefault()
 
-    const country =  document.querySelector("#country").value
+    const country = document.querySelector("#country").value
     const genre = document.querySelector("#genre").value
 
     let url = "https://namefake.com/"
 
-    if(country !== ""){
+    if (country !== "") {
         url += (country + "/")
     } else {
         url += "random/"
     }
 
-    if(genre !== ""){
+    if (genre !== "") {
         url += (genre + "/")
     } else {
         url += "random/"
@@ -25,15 +25,29 @@ function loadNames (e){
 
     const xhr = new XMLHttpRequest()
 
-    xhr.open("GET" , url , true)
+    xhr.open("GET", url, true)
 
-    xhr.unload = function (){
-        if(this.status === 200 && this.readyState === 4){
+    xhr.unload = function () {
+        if (this.status === 200 && this.readyState === 4) {
             console.log(this.responseText);
+            const response = JSON.parse(thes.responseText)
+            console.log(response);
+            let output = ""
+            response.forEach(perople => {
+
+                output += `
+                <ul>
+                 <li>ID: ${person.id}</li>
+                 <li>Name: ${person.name}</li>
+                 <li>Job: ${person.job}</li>
+                </ul>
+                `
+                console.log(output);
+            });
+            document.querySelector("#result").innerHTML = output
         }
     }
 
     xhr.send()
 
 }
-
