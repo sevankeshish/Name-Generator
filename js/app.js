c11e5e280a30927a218270b0f6a4341cb01043b7
sevan keshish <35175803+sevankeshish@users.noreply.github.com>
2022-03-21 00:19:34 +0330

    blocking the server because of the fake api :(

--- js/app.js
+++ js/app.js
@@ -1,53 +1,58 @@
-document.querySelector("#generate-names").addEventListener("submit", loadNames)
+//access to the form values
+document.querySelector("#generate-names").addEventListener("submit", loadNames);
 
+//read the values and generate URL
 function loadNames(e) {
-    e.preventDefault()
+  e.preventDefault();
 
-    const country = document.querySelector("#country").value
-    const genre = document.querySelector("#genre").value
+  const country = document.querySelector("#country").value;
+  const gender = document.querySelector("#gender").value;
+  // console.log(country);
+  // console.log(gender);
 
-    let url = "https://namefake.com/"
+  //generate the URL
+  let url = "https://api.namefake.com/";
 
-    if (country !== "") {
-        url += (country + "/")
-    } else {
-        url += "random/"
-    }
+  //check the country value
+  if (country !== "") {
+    url += country + "/";
+  } else {
+    url += "random/";
+  }
 
-    if (genre !== "") {
-        url += (genre + "/")
-    } else {
-        url += "random/"
-    }
-    // console.log(country);
-    // console.log(genre);
-    // console.log(url);
+  //check the gender value
+  if (gender !== "") {
+    url += gender + "/";
+  } else {
+    url += "random/";
+  }
+  // console.log(country);
+  // console.log(gender);
+  // console.log(url);
 
-    const xhr = new XMLHttpRequest()
+  const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", url, true)
+  xhr.open("GET", url, true);
 
-    xhr.unload = function () {
-        if (this.status === 200 && this.readyState === 4) {
-            console.log(this.responseText);
-            const response = JSON.parse(thes.responseText)
-            console.log(response);
-            let output = ""
-            response.forEach(perople => {
-
-                output += `
+  xhr.onload = function () {
+    if (this.status === 200 && this.readyState === 4) {
+      console.log(this.responseText);
+      const response = JSON.parse(thes.responseText);
+      console.log(response);
+      let output = "";
+      response.forEach((perople) => {
+        output += `
                 <ul>
                  <li>ID: ${person.id}</li>
                  <li>Name: ${person.name}</li>
                  <li>Job: ${person.job}</li>
                 </ul>
-                `
-                console.log(output);
-            });
-            document.querySelector("#result").innerHTML = output
-        }
+                `;
+        console.log(output);
+      });
+      document.querySelector("#result").innerHTML = output;
     }
+  };
 
-    xhr.send()
-
+  xhr.send();
 }
